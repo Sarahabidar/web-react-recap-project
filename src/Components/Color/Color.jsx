41b1c494cc66cdaad85c7171c8395c4342a712d8
sara <sarah.abiidar@gmail.com>
2024-06-05 12:32:24 +0200

    delete a color with declare a function

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,17 +1,39 @@
+import React, { useState } from "react";
 import "./Color.css";
 
-export default function Color({ color }) {
+export default function Color({ color, onDelete }) {
+  const [showConfirmation, setshowConfirmation] = useState(false);
+
+  const handleDelete = () => {
+    setshowConfirmation(true);
+  };
+
+  const cancelDelete = () => {
+    setshowConfirmation(false);
+  };
+  const confirmDelete = () => {
+    onDelete(color.id);
+  };
+
   return (
     <div
       className="color-card"
       style={{
         background: color.hex,
-        color: color.contrastText,
+        color: color.contrastsText,
       }}
     >
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <button onClick={handleDelete}>DELETE</button>
+      {showConfirmation ? (
+        <div>
+          <p className="color-card-hightlight">Really delete?</p>
+          <button onClick={cancelDelete}>CANCEL</button>
+          <button onClick={confirmDelete}>DELETE</button>
+        </div>
+      ) : null}
     </div>
   );
 }
