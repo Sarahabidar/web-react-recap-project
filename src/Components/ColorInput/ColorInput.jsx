8f08a537127bb46901ca83ff619829632a4d5039
sara <sarah.abiidar@gmail.com>
2024-06-05 20:54:58 +0200

    add style to ColorInput and create colorIput.css

--- src/Components/ColorInput/ColorInput.jsx
+++ src/Components/ColorInput/ColorInput.jsx
@@ -1,3 +1,4 @@
+import "./ColorInput.css";
 export default function ColorInput({ label, value, onChange }) {
   const handleTextChange = (event) => {
     onChange(event.target.value);
@@ -8,7 +9,7 @@ export default function ColorInput({ label, value, onChange }) {
   };
   return (
     <div>
-      <label>{label}</label>
+      <label className="color-form-input">{label}</label>
       <input type="text" value={value} onChange={handleTextChange}></input>
       <input type="color" value={value} onChange={handleColorChange}></input>
     </div>
